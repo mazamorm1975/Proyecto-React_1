4ecd4464c00709a35af67b0c28eae6c0ff9e2fbe
mazamora1975 <mzamora114@hotmail.com>
2023-04-24 13:05:35 -0600

    Se anexan los archivos: App.jsx, index.css y react.svg

---
+++ public/assets/App.jsx
@@ -0,0 +1,18 @@
+ export function App (){
+   return (
+    <article style={{display: 'flex', alignItems:'center', color:'#fff'}}>
+        <header>
+            <img alt="El avatar de Mario-Z" src="	https://campus.unadmexico.mx/pluginfile.php/22807/user/icon/boost/f2?rev=3650719"/>
+            <div>
+               <strong>My React App</strong> 
+               <span>@MarioZ</span>
+            </div>
+        </header>
+        <aside>
+            <button> 
+                Seguir
+            </button>
+        </aside>
+    </article>
+   )
+}
