db8cb54cdbb5fe8145fd4d62379eed1125ae1948
mazamora1975 <mzamora114@hotmail.com>
2023-04-27 10:33:44 -0600

    Se agregan cambios en el proyecto: aplicacion-react-1.0

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,18 +1,12 @@
- export function App (){
-   return (
-    <article style={{display: 'flex', alignItems:'center', color:'#fff'}}>
-        <header>
-            <img alt="El avatar de Mario-Z" src="	https://campus.unadmexico.mx/pluginfile.php/22807/user/icon/boost/f2?rev=3650719"/>
-            <div>
-               <strong>My React App</strong> 
-               <span>@MarioZ</span>
-            </div>
-        </header>
-        <aside>
-            <button> 
-                Seguir
-            </button>
-        </aside>
-    </article>
-   )
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterCard.jsx";
+
+export function App() {
+  return (
+    <>
+      <TwitterFollowCard userName="lapicito" name="Mario Zamora" name2="Mario-Z" />
+      <TwitterFollowCard userName="mujer.jpg.jpg" name="Patricia Aburto" name2="Paty-Porn" />
+      <TwitterFollowCard userName="MisHijas.jpg" name="Hijas" name2="Bendiciones" />
+    </>
+  );
 }
