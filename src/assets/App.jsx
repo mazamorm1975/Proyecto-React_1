943de4e2732079099dac62f20b21ebdd311f8778
mazamora1975 <mzamora114@hotmail.com>
2023-04-27 13:26:56 -0600

    Se anexa foto de mujer rubia

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <>
       <TwitterFollowCard userName="lapicito" name="Mario Zamora" name2="Mario-Z" />
-      <TwitterFollowCard userName="mujer.jpg.jpg" name="Patricia Aburto" name2="Paty-Porn" />
+      <TwitterFollowCard userName="paty" name="Patricia Aburto" name2="Paty-Porn" />
       <TwitterFollowCard userName="MisHijas.jpg" name="Hijas" name2="Bendiciones" />
       <TwitterFollowCard userName="MarioZamora.jpg" name="Mario" name2="Cherokeexxx" />
       <TwitterFollowCard userName="don_ramon" name="Don Ramon" name2="Don_Ramon2023" />
