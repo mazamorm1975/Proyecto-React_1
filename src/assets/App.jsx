e2b7bae520e2c303a059a4198bf2d01a64f880c8
mazamora1975 <mzamora114@hotmail.com>
2023-04-27 11:58:34 -0600

    Se actualiza twitter de Mario Zamora

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -4,7 +4,6 @@ import { TwitterFollowCard } from "./TwitterCard.jsx";
 export function App() {
   return (
     <>
-<<<<<<< HEAD
       <TwitterFollowCard
         userName="lapicito"
         name="Mario Zamora"
@@ -20,12 +19,13 @@ export function App() {
         name="Hijas"
         name2="Bendiciones"
       />
-=======
-      <TwitterFollowCard userName="lapicito" name="Mario Zamora" name2="Mario-Z" />
-      <TwitterFollowCard userName="mujer.jpg.jpg" name="Patricia Aburto" name2="Paty-Porn" />
-      <TwitterFollowCard userName="MisHijas.jpg" name="Hijas" name2="Bendiciones" />
-      <TwitterFollowCard userName="MarioZamora.jpg" name="Mario" name2="Cherokeexxx" />
->>>>>>> 9a44b2615afacc6cf3ea3162a464c5af3486a0b4
+
+      <TwitterFollowCard
+        userName="MarioZamora"
+        name="Mario Zamora Martinez"
+        name2="Cherokeexxx"
+      />
+
     </>
   );
 }
