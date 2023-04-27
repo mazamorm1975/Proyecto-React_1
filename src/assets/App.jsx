a7f014bdd7cded12292ec1c20d1d3a7cdf151c43
mazamora1975 <mzamora114@hotmail.com>
2023-04-27 10:45:59 -0600

    Cambios desde rama main

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -4,9 +4,21 @@ import { TwitterFollowCard } from "./TwitterCard.jsx";
 export function App() {
   return (
     <>
-      <TwitterFollowCard userName="lapicito" name="Mario Zamora" name2="Mario-Z" />
-      <TwitterFollowCard userName="mujer.jpg.jpg" name="Patricia Aburto" name2="Paty-Porn" />
-      <TwitterFollowCard userName="MisHijas.jpg" name="Hijas" name2="Bendiciones" />
+      <TwitterFollowCard
+        userName="lapicito"
+        name="Mario Zamora"
+        name2="Mario-Z"
+      />
+      <TwitterFollowCard
+        userName="mujer.jpg.jpg"
+        name="Patricia Aburto"
+        name2="Paty-Porn"
+      />
+      <TwitterFollowCard
+        userName="MisHijas.jpg"
+        name="Hijas"
+        name2="Bendiciones"
+      />
     </>
   );
 }
