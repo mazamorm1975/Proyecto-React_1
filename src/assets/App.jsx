9a44b2615afacc6cf3ea3162a464c5af3486a0b4
mazamora1975 <mzamora114@hotmail.com>
2023-04-27 11:37:12 -0600

    Se agrega actualizacion en el archivo App.jsx

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -7,6 +7,7 @@ export function App() {
       <TwitterFollowCard userName="lapicito" name="Mario Zamora" name2="Mario-Z" />
       <TwitterFollowCard userName="mujer.jpg.jpg" name="Patricia Aburto" name2="Paty-Porn" />
       <TwitterFollowCard userName="MisHijas.jpg" name="Hijas" name2="Bendiciones" />
+      <TwitterFollowCard userName="MarioZamora.jpg" name="Mario" name2="Cherokeexxx" />
     </>
   );
 }
